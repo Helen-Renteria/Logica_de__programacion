2e2b5255f28d2f9c832acf8b19b56f86caf4b8dc
Helen-Renteria <helenfarinarenteria444@gmail.com>
2024-11-12 21:51:21 -0500

    JS

---
+++ JS/js/script.js
@@ -0,0 +1,36 @@
+/*let temperatura = 30; 
+if(temperatura>=30){
+    console.log("hace mucho calor.");
+
+}else if(temperatura>=20){
+    console.log("el clima esta agradable."); 
+}else{
+    console.log("hace frio. "); 
+} */
+/*
+const mascota = "perro";
+switch(mascota){
+    case "lagarto":
+        console.log("tengo un lagarto");
+        break; 
+    case "leon":
+        console.log("tengo un lagarto");
+        break; 
+    case "perro":
+        console.log("tengo un lagarto");
+        break; 
+    default: 
+    console.log("no tengo mascota");
+    
+}
+*/
+// ternario 
+// condicion? valorsiesvedadero: calorsiesfalso
+/*let edad=20;
+let resultado= (edad>=18)?"mayor de edad": "menor de edad";
+console.log(resultado);
+*/
+
+let numero= 7; 
+let tipo= (numero % 2===0)? "par": "impar"; 
+console.log(tipo);
